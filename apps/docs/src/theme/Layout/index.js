09e6c1c639d8a3c9fad92ca522b661a34466159d
erzan <do.earljan@gmail.com>
2026-03-18 21:08:24 +0800

    Fix analytics not defined error causing compilation to break

--- apps/docs/src/theme/Layout/index.js
+++ apps/docs/src/theme/Layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '@theme-original/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function LayoutWrapper(props) {
   return (
